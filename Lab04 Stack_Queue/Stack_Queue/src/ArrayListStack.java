8ca14a2ee5a534b380165d98ec01b404f4a593fe
bosswt <boss.wt13@gmail.com>
2021-02-20 15:42:00 +0700

    Lab04 Stack_Queue

--- Lab04 Stack_Queue/Stack_Queue/src/ArrayListStack.java
+++ Lab04 Stack_Queue/Stack_Queue/src/ArrayListStack.java
@@ -6,7 +6,7 @@ public class ArrayListStack implements Stack {
 	// implement a default constructor and all methods from interface Stack.
 	// Additional methods maybe required in order to run tests.
 	public ArrayListStack() {
-
+		a = new ArrayList<Integer>();
 	}
 
 	public ArrayList<Integer> getA() {
@@ -15,32 +15,36 @@ public class ArrayListStack implements Stack {
 
 	@Override
 	public boolean isEmpty() {
-
+		if (a.isEmpty())
+			return true;
+		return false;
 	}
 
 	@Override
 	public boolean isFull() {
-
+		if (a.size() >= 100000)
+			return true;
+		return false;
 	}
 
 	@Override
 	public void makeEmpty() {
-
+		a.clear();
 	}
 
 	@Override
 	public int top() throws Exception {
-
+		return a.get(0);
 	}
 
 	@Override
 	public void pop() throws Exception {
-
+		a.remove(0);
 	}
 
 	@Override
 	public void push(int data) throws Exception {
-
+		a.add(0, data);
 	}
 
 }
--- Lab04 Stack_Queue/Stack_Queue/src/UseStack.java
+++ Lab04 Stack_Queue/Stack_Queue/src/UseStack.java
@@ -3,7 +3,26 @@ public class UseStack {
 
 	// implement this method.
 	public static Stack sort(Stack s) throws Exception {
-
+		ArrayListStack answer = new ArrayListStack();
+		while (!s.isEmpty()) {
+			int sTop = s.top();
+			if (answer.isEmpty() || sTop <= answer.top())
+				answer.push(sTop);
+			else {
+				ArrayListStack tempStack = new ArrayListStack();
+				while (!answer.isEmpty() && sTop > answer.top()) {
+					tempStack.push(answer.top());
+					answer.pop();
+				}
+				answer.push(sTop);
+				while (!tempStack.isEmpty()) {
+					answer.push(tempStack.top());
+					tempStack.pop();
+				}
+			}
+			s.pop();
+		}
+		return answer;
 	}
 
 }
