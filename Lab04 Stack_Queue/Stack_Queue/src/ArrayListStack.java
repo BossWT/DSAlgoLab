5612e5517433e97fe53832e4cd65a6815027d0f6
bosswt <boss.wt13@gmail.com>
2021-02-20 14:54:47 +0700

    Rename Doubly LikedList to Lab03

---
+++ Lab04 Stack_Queue/Stack_Queue/src/ArrayListStack.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class ArrayListStack implements Stack {
+	private ArrayList<Integer> a;
+
+	// implement a default constructor and all methods from interface Stack.
+	// Additional methods maybe required in order to run tests.
+	public ArrayListStack() {
+
+	}
+
+	public ArrayList<Integer> getA() {
+		return a;
+	}
+
+	@Override
+	public boolean isEmpty() {
+
+	}
+
+	@Override
+	public boolean isFull() {
+
+	}
+
+	@Override
+	public void makeEmpty() {
+
+	}
+
+	@Override
+	public int top() throws Exception {
+
+	}
+
+	@Override
+	public void pop() throws Exception {
+
+	}
+
+	@Override
+	public void push(int data) throws Exception {
+
+	}
+
+}
---
+++ Lab04 Stack_Queue/Stack_Queue/src/UseStack.java
@@ -0,0 +1,9 @@
+
+public class UseStack {
+
+	// implement this method.
+	public static Stack sort(Stack s) throws Exception {
+
+	}
+
+}
